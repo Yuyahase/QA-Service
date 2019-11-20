fb1e61e418ed343e10570c7bbb384e578efd8d86
Yuya <haseyuy@gmail.com>
2019-11-20 21:46:52 +0900

    create home view

--- resources/views/layout/main.blade.php
+++ resources/views/layout/main.blade.php
@@ -11,6 +11,7 @@
     <div class="container">
         @yield('content')
     </div>
+    @include('layout.partials._footer')
 </body>
 <script src="{{url('js/jquery-3.2.1.min.js')}}"></script>
 <script src="{{url('js/bootstrap.js')}}"></script>
